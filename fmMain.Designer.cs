1f8dd727964928d7f568da5eb72eed4a5e7765f5
Stian Hanger <pdnagilum@gmail.com>
2016-09-10 23:49:20 +0200

    Initial commit

---
+++ fmMain.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace iisewars {
+	partial class fmMain {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fmMain));
+			this.label1 = new System.Windows.Forms.Label();
+			this.tbSolutionFile = new System.Windows.Forms.TextBox();
+			this.btSolutionFile = new System.Windows.Forms.Button();
+			this.btGo = new System.Windows.Forms.Button();
+			this.lbStep1 = new System.Windows.Forms.Label();
+			this.lbStep2 = new System.Windows.Forms.Label();
+			this.lbStep3 = new System.Windows.Forms.Label();
+			this.pbStep1 = new System.Windows.Forms.PictureBox();
+			this.pbStep2 = new System.Windows.Forms.PictureBox();
+			this.pbStep3 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pbStep1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbStep2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbStep3)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(67, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Solution File:";
+			// 
+			// tbSolutionFile
+			// 
+			this.tbSolutionFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbSolutionFile.Location = new System.Drawing.Point(15, 25);
+			this.tbSolutionFile.Name = "tbSolutionFile";
+			this.tbSolutionFile.Size = new System.Drawing.Size(312, 20);
+			this.tbSolutionFile.TabIndex = 1;
+			// 
+			// btSolutionFile
+			// 
+			this.btSolutionFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btSolutionFile.Location = new System.Drawing.Point(333, 22);
+			this.btSolutionFile.Name = "btSolutionFile";
+			this.btSolutionFile.Size = new System.Drawing.Size(42, 26);
+			this.btSolutionFile.TabIndex = 2;
+			this.btSolutionFile.Text = "...";
+			this.btSolutionFile.UseVisualStyleBackColor = true;
+			this.btSolutionFile.Click += new System.EventHandler(this.btSolutionFile_Click);
+			// 
+			// btGo
+			// 
+			this.btGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btGo.Location = new System.Drawing.Point(381, 22);
+			this.btGo.Name = "btGo";
+			this.btGo.Size = new System.Drawing.Size(42, 26);
+			this.btGo.TabIndex = 3;
+			this.btGo.Text = "Go";
+			this.btGo.UseVisualStyleBackColor = true;
+			this.btGo.Click += new System.EventHandler(this.btGo_Click);
+			// 
+			// lbStep1
+			// 
+			this.lbStep1.AutoSize = true;
+			this.lbStep1.ForeColor = System.Drawing.SystemColors.ControlDark;
+			this.lbStep1.Location = new System.Drawing.Point(44, 69);
+			this.lbStep1.Name = "lbStep1";
+			this.lbStep1.Size = new System.Drawing.Size(172, 13);
+			this.lbStep1.TabIndex = 4;
+			this.lbStep1.Text = "Configure IIS Express with Local IP";
+			// 
+			// lbStep2
+			// 
+			this.lbStep2.AutoSize = true;
+			this.lbStep2.ForeColor = System.Drawing.SystemColors.ControlDark;
+			this.lbStep2.Location = new System.Drawing.Point(44, 92);
+			this.lbStep2.Name = "lbStep2";
+			this.lbStep2.Size = new System.Drawing.Size(192, 13);
+			this.lbStep2.TabIndex = 5;
+			this.lbStep2.Text = "Enable Access to the URL for All Users";
+			// 
+			// lbStep3
+			// 
+			this.lbStep3.AutoSize = true;
+			this.lbStep3.ForeColor = System.Drawing.SystemColors.ControlDark;
+			this.lbStep3.Location = new System.Drawing.Point(44, 115);
+			this.lbStep3.Name = "lbStep3";
+			this.lbStep3.Size = new System.Drawing.Size(169, 13);
+			this.lbStep3.TabIndex = 6;
+			this.lbStep3.Text = "Open the Port in Windows Firewall";
+			// 
+			// pbStep1
+			// 
+			this.pbStep1.Image = global::iisewars.Properties.Resources.check_icon;
+			this.pbStep1.Location = new System.Drawing.Point(22, 67);
+			this.pbStep1.Name = "pbStep1";
+			this.pbStep1.Size = new System.Drawing.Size(16, 16);
+			this.pbStep1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pbStep1.TabIndex = 8;
+			this.pbStep1.TabStop = false;
+			this.pbStep1.Visible = false;
+			// 
+			// pbStep2
+			// 
+			this.pbStep2.Image = global::iisewars.Properties.Resources.check_icon;
+			this.pbStep2.Location = new System.Drawing.Point(22, 90);
+			this.pbStep2.Name = "pbStep2";
+			this.pbStep2.Size = new System.Drawing.Size(16, 16);
+			this.pbStep2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pbStep2.TabIndex = 9;
+			this.pbStep2.TabStop = false;
+			this.pbStep2.Visible = false;
+			// 
+			// pbStep3
+			// 
+			this.pbStep3.Image = global::iisewars.Properties.Resources.check_icon;
+			this.pbStep3.Location = new System.Drawing.Point(22, 113);
+			this.pbStep3.Name = "pbStep3";
+			this.pbStep3.Size = new System.Drawing.Size(16, 16);
+			this.pbStep3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pbStep3.TabIndex = 10;
+			this.pbStep3.TabStop = false;
+			this.pbStep3.Visible = false;
+			// 
+			// fmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(435, 151);
+			this.Controls.Add(this.pbStep3);
+			this.Controls.Add(this.pbStep2);
+			this.Controls.Add(this.pbStep1);
+			this.Controls.Add(this.lbStep3);
+			this.Controls.Add(this.lbStep2);
+			this.Controls.Add(this.lbStep1);
+			this.Controls.Add(this.btGo);
+			this.Controls.Add(this.btSolutionFile);
+			this.Controls.Add(this.tbSolutionFile);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "fmMain";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "IIS Express WebApp Remote Share";
+			this.Load += new System.EventHandler(this.fmMain_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pbStep1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbStep2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbStep3)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox tbSolutionFile;
+		private System.Windows.Forms.Button btSolutionFile;
+		private System.Windows.Forms.Button btGo;
+		private System.Windows.Forms.Label lbStep1;
+		private System.Windows.Forms.Label lbStep2;
+		private System.Windows.Forms.Label lbStep3;
+		private System.Windows.Forms.PictureBox pbStep1;
+		private System.Windows.Forms.PictureBox pbStep2;
+		private System.Windows.Forms.PictureBox pbStep3;
+	}
+}
+
---
+++ fmMain.cs
@@ -0,0 +1,235 @@
+﻿using NetFwTypeLib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace iisewars {
+	public partial class fmMain : Form {
+		public fmMain() {
+			InitializeComponent();
+		}
+
+		private void fmMain_Load(object sender, EventArgs e) {
+			this.tbSolutionFile.Text = Properties.Settings.Default.SolutionFile;
+		}
+
+		private void btSolutionFile_Click(object sender, EventArgs e) {
+			var dialog = new OpenFileDialog {
+				CheckFileExists = true,
+				CheckPathExists = true,
+				FileName = this.tbSolutionFile.Text.Trim(),
+				Filter = "Solution Files (.sln)|*.sln",
+				InitialDirectory = Environment.CurrentDirectory,
+				Multiselect = false,
+				Title = "Select Solution File for WebApp to Use"
+			};
+
+			if (dialog.ShowDialog(this) != DialogResult.OK)
+				return;
+
+			this.tbSolutionFile.Text = dialog.FileName;
+
+			Properties.Settings.Default.SolutionFile = dialog.FileName;
+			Properties.Settings.Default.Save();
+		}
+
+		private void btGo_Click(object sender, EventArgs e) {
+			if (!File.Exists(this.tbSolutionFile.Text.Trim())) {
+				MessageBox.Show(
+					"Selected solution file does not exist.",
+					"Error",
+					MessageBoxButtons.OK,
+					MessageBoxIcon.Error);
+
+				return;
+			}
+
+			// Get localhost IP.
+			var ipAddress = string.Empty;
+
+			foreach (var ip in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+				if (ip.AddressFamily == AddressFamily.InterNetwork)
+					ipAddress = ip.ToString();
+
+			if (string.IsNullOrWhiteSpace(ipAddress)) {
+				MessageBox.Show(
+					"Could not find IP for localhost.",
+					"Error",
+					MessageBoxButtons.OK,
+					MessageBoxIcon.Error);
+
+				return;
+			}
+
+			// Get WebApp port.
+			var assemblyName = string.Empty;
+			var webAppPort = 0;
+			var solutionLines = File.ReadAllLines(this.tbSolutionFile.Text.Trim());
+			var projects = new List<string>();
+			var path = this.tbSolutionFile.Text.Trim();
+
+			path = path.Substring(0, path.LastIndexOf(@"\", StringComparison.InvariantCultureIgnoreCase));
+
+			foreach (var line in solutionLines) {
+				if (!line.Contains(".csproj"))
+					continue;
+
+				var filename = line.Substring(0, line.IndexOf(".csproj", StringComparison.InvariantCultureIgnoreCase) + ".csproj".Length);
+				filename = filename.Substring(filename.LastIndexOf("\"", StringComparison.InvariantCultureIgnoreCase) + 1);
+
+				projects.Add(
+					string.Format(
+						"{0}\\{1}",
+						path,
+						filename));
+			}
+
+			foreach (var pathname in projects) {
+				var projectLines = File.ReadAllLines(pathname);
+
+				foreach (var line in projectLines) {
+					if (line.Contains("AssemblyName") &&
+					    line.Contains("/AssemblyName")) {
+						assemblyName = line.Substring(line.IndexOf("<AssemblyName>", StringComparison.InvariantCultureIgnoreCase) + "<AssemblyName>".Length);
+						assemblyName = assemblyName.Substring(0, assemblyName.IndexOf("<", StringComparison.InvariantCultureIgnoreCase));
+					}
+
+					if (line.Contains("DevelopmentServerPort") &&
+					    line.Contains("/DevelopmentServerPort")) {
+						var developmentServerPort = line.Substring(line.IndexOf("<DevelopmentServerPort>", StringComparison.InvariantCultureIgnoreCase) + "<DevelopmentServerPort>".Length);
+						developmentServerPort = developmentServerPort.Substring(0, developmentServerPort.IndexOf("<", StringComparison.InvariantCultureIgnoreCase));
+						int.TryParse(developmentServerPort, out webAppPort);
+					}
+				}
+			}
+
+			if (string.IsNullOrWhiteSpace(assemblyName)) {
+				MessageBox.Show(
+					"Could not determine webapp assemblyname from solution.",
+					"Error",
+					MessageBoxButtons.OK,
+					MessageBoxIcon.Error);
+
+				return;
+			}
+
+			if (webAppPort == 0) {
+				MessageBox.Show(
+					"Could not determine webapp port from solution.",
+					"Error",
+					MessageBoxButtons.OK,
+					MessageBoxIcon.Error);
+
+				return;
+			}
+
+			// Step 1: Configure IIS Express with Local IP.
+			var outputLines = new List<string>();
+			var configFile = string.Format(
+				"{0}\\IISExpress\\config\\applicationhost.config",
+				Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+			var configLines = File.ReadAllLines(configFile);
+			var foundEntry = configLines.Any(line => line.Contains(string.Format("{0}:{1}:{0}", ipAddress, webAppPort)));
+
+			if (!foundEntry) {
+				foreach (var line in configLines) {
+					outputLines.Add(
+						line);
+
+					if (line.Contains(string.Format("*:{0}:localhost", webAppPort)))
+						outputLines.Add(
+							string.Format("                    <binding protocol=\"http\" bindingInformation=\"{0}:{1}:{0}\" />", ipAddress, webAppPort));
+				}
+
+				try {
+					File.WriteAllLines(
+						configFile,
+						outputLines.ToArray());
+				}
+				catch (Exception ex) {
+					MessageBox.Show(
+						ex.Message,
+						"Error",
+						MessageBoxButtons.OK,
+						MessageBoxIcon.Error);
+
+					return;
+				}
+			}
+
+			this.pbStep1.Visible = true;
+			this.lbStep1.ForeColor = SystemColors.ControlText;
+
+			Application.DoEvents();
+			Thread.Sleep(100);
+
+			// Step 2: Enable Access to the URL for All Users.
+			try {
+				Process.Start(
+					new ProcessStartInfo {
+						FileName = string.Format("{0}\\System32\\netsh.exe", Environment.ExpandEnvironmentVariables("%SystemRoot%")),
+						Arguments = string.Format("http add urlacl url=http://{0}:{1}/ user=everyone", ipAddress, webAppPort),
+						Verb = "runas",
+						WindowStyle = ProcessWindowStyle.Hidden
+					});
+			}
+			catch (Exception ex) {
+				MessageBox.Show(
+					ex.Message,
+					"Error",
+					MessageBoxButtons.OK,
+					MessageBoxIcon.Error);
+
+				return;
+			}
+
+			this.pbStep2.Visible = true;
+			this.lbStep2.ForeColor = SystemColors.ControlText;
+
+			Application.DoEvents();
+			Thread.Sleep(100);
+
+			// Step 3: Open the Port in Windows Firewall.
+			try {
+				var fwMgrType = Type.GetTypeFromProgID("HNetCfg.FwMgr", false);
+				var fwMgr = (INetFwMgr) Activator.CreateInstance(fwMgrType);
+				var fwPorts = fwMgr.LocalPolicy.CurrentProfile.GloballyOpenPorts;
+				var fwPortEnumerator = fwPorts.GetEnumerator();
+				var fwAppPortFound = false;
+				INetFwOpenPort fwAppPort = null;
+
+				while (fwPortEnumerator.MoveNext()) {
+					fwAppPort = (INetFwOpenPort) fwPortEnumerator.Current;
+
+					if (fwAppPort.Port != webAppPort)
+						continue;
+
+					fwAppPortFound = true;
+					break;
+				}
+
+				if (!fwAppPortFound)
+					fwPorts.Add(fwAppPort);
+			}
+			catch (Exception ex) {
+				MessageBox.Show(
+					ex.Message,
+					"Error",
+					MessageBoxButtons.OK,
+					MessageBoxIcon.Error);
+
+				return;
+			}
+
+			this.pbStep3.Visible = true;
+			this.lbStep3.ForeColor = SystemColors.ControlText;
+		}
+	}
+}
